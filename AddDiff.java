cc3e43eab527254a307c9f0e231403e088f04ff9
Sachin20187 <sachinkumar.20187@gmail.com>
2024-09-02 23:59:03 +0530

    First Commit

---
+++ AddDiff.java
@@ -0,0 +1,12 @@
+public class AddDiff {
+    public static void main(String[] args) {
+        int a = 15, b = 10;
+        int add = a+b;
+        int diff = a-b;
+
+        System.out.println(add);
+        System.out.println(diff);
+
+    }
+    
+}
