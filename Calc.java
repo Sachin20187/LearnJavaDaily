cf17e2d1783f5224d31bd1434f63fa076354e2db
Sachin20187 <sachinkumar.20187@gmail.com>
2024-09-03 14:05:31 +0530

    Create Calc.java
    update

---
+++ Calc.java
@@ -0,0 +1,21 @@
+public class Calc {
+    public static void main(String[] args) {
+        int a = 15, b = 10;
+        int add = a+b;
+        int diff = a-b;
+        int prod = a*b;
+        int div = a/b;
+        int mod = a%b;
+        System.out.println(add);
+        System.out.println(diff);
+        System.out.println(prod);
+        System.out.println(div);
+        System.out.println(mod);
+
+        float g = 10.0f, f = 2.5f;
+        float res = g/f;
+        System.out.println(res);
+
+    }
+    
+}
