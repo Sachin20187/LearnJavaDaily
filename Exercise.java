3b46d99cbcb85ac35c588ca0909eb25881a5754d
Sachin20187 <sachinkumar.20187@gmail.com>
2024-09-03 14:07:28 +0530

    First

---
+++ Exercise.java
@@ -0,0 +1,31 @@
+public class Exercise {
+    public static void main(String args[]) {
+        // Define your variables below this line
+        int int_number= 1000;
+        float float_number= 10.292f;
+        double double_number=0.00000000000512365123;
+        char char_name= 'N';
+        boolean bool_accept= true;
+
+
+        System.out.println("Value of Int is: "+ int_number);
+        // print your int variable here
+        System.out.println(int_number);
+
+        System.out.println("Value of Float is: "+ float_number);
+        // print your float variable here
+        System.out.println(float_number);
+
+        System.out.println("Value of Double is: "+ double_number);
+        // print your double variable here
+        System.out.println(double_number);
+
+        System.out.println("Value of Char is: "+ char_name);
+        // print your char variable here
+        System.out.println(char_name);
+
+        System.out.println("Value of Bool is: "+ bool_accept);
+        // print bool double variable here
+        System.out.println(bool_accept);
+   }
+}
